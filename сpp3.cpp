93b3a4a3bf1f80beae19d41cf95f3cfaf968183c
VanDokk <48243610+VanDokk@users.noreply.github.com>
2019-05-01 18:04:27 +0300

    Add files via upload

---
+++ сpp3.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	const int size = 6; // 6 символов, т.к. последнее место отводится под нуль-терминатор
+	char str[size];
+	std::cout << "Введите слово из пяти символов" << std::endl << std::endl;
+	std::cin >> str;
+
+	bool flag = false;
+	if ((str[0] == str[size - 2]) && (str[1] == str[size - 3]))
+		std::cout << "Слово является полиндромом";
+	else
+		std::cout << "Слово не является полиндромом";
+
+}
+
